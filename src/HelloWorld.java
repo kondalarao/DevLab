2326ead667e39a29723ecd4c2c556554512a6dbb
Kondala Rao Thota <kondalarao.thota@gmail.com>
2017-04-06 12:38:47 +0530

    Added project

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * Created by thotk on 06-04-2017.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Prints "Hello, World" in the terminal window.
+        System.out.println("Hello, World");
+    }
+}
